7687664f7b0e4bb74e1a78c87982429def219a40
KyleAstudillo <kyle.astudillo@yahoo.com>
2017-01-28 16:07:49 -0800

    Added two activities and transitions

--- app/src/main/java/com/example/kyle/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kyle/myapplication/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.kyle.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button questionButton = (Button)findViewById(R.id.question_button);
+        questionButton.setOnClickListener(this);
+        Button pictureButton = (Button)findViewById(R.id.picture_button);
+        pictureButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        if (v.getId() == R.id.question_button) {
+            startActivity(new Intent(MainActivity.this, QuestionActivity.class));
+        }else if (v.getId() == R.id.picture_button){
+            startActivity(new Intent(MainActivity.this, PictureActivity.class));
+        }
     }
 }
